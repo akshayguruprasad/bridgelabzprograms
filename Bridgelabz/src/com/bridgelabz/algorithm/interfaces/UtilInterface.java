d449c09431ae8f56785d72673a414c5f15715e9e
Akshaygpag <akshayguruprasad@gmail.com>
2018-06-22 20:37:31 +0530

    jdbc impl done

--- Bridgelabz/src/com/bridgelabz/algorithm/interfaces/UtilInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.algorithm.interfaces;
-
-public interface UtilInterface {
-	String ISANAGRAM="Strings are anagram";
-	String ISNOTANAGRAM="Strings are not anagram";
-	String ISPALINDROME=" palindrome";
-	String ISNOTPALINDROME=" not palindrome";
-	
-}
